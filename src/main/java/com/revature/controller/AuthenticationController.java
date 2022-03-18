e378c03f7304fe0c1ea252c3439edffcc162557b
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-03-18 08:56:29 -0400

    Strated with DTO

--- src/main/java/com/revature/controller/AuthenticationController.java
+++ src/main/java/com/revature/controller/AuthenticationController.java
@@ -1,4 +1,27 @@
 package com.revature.controller;
 
-public class AuthenticationController {
+import com.revature.model.User;
+import com.revature.service.UserService;
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+
+public class AuthenticationController implements Controller {
+
+    private UserService userService;
+
+    public AuthenticationController(){
+        this.userService= new UserService();
+    }
+
+    private Handler login = (ctx) -> {
+        System.out.println("Login endpoint invoked");
+
+        User loginInfo = ctx.bodyAsClass(User.class);
+        System.out.println(loginInfo);
+    };
+
+    @Override
+    public void mapEndpoints(Javalin app) {
+    app.post("/login", login);
+    }
 }
--- src/main/java/com/revature/main/Driver.java
+++ src/main/java/com/revature/main/Driver.java
@@ -1,23 +1,37 @@
 package com.revature.main;
 
+import com.revature.controller.AuthenticationController;
+import com.revature.controller.Controller;
 import com.revature.dao.UserDao;
 import com.revature.utility.ConnectionUtility;
+import io.javalin.Javalin;
 
 import java.sql.SQLException;
 
 public class Driver {
 
     public static void main(String[] args)  {
-        UserDao dao = new UserDao();
-         try{
-             System.out.println(dao.getUserByUsernameAndPassword("ryan_jordan","password123"));
-         } catch (SQLException e) {
-             e.printStackTrace();
-         }
+//        UserDao dao = new UserDao();
+//         try{
+//             System.out.println(dao.getUserByUsernameAndPassword("ryan_jordan","password123"));
+//         } catch (SQLException e) {
+//             e.printStackTrace();
+//         }
 //        try {
 //            ConnectionUtility.getConnection();
 //        } catch (SQLException e) {
 //            e.printStackTrace();
 //        }
-    }
+        Javalin app = Javalin.create();
+        map(app, new AuthenticationController());
+
+          app.start(8081);
+
+        }
+
+        public static void map(Javalin app, Controller... controllers){
+            for(Controller c : controllers){
+                c.mapEndpoints(app);
+            }
+        }
 }
