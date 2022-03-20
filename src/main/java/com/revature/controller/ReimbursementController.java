98dc8c331076a3e64317763a4762609a0733ced9
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-03-20 13:59:47 -0400

    tested jwt

--- src/main/java/com/revature/controller/ReimbursementController.java
+++ src/main/java/com/revature/controller/ReimbursementController.java
@@ -1,12 +1,18 @@
 package com.revature.controller;
 
 import com.revature.model.User;
+import com.revature.service.JWTService;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 import io.javalin.http.UnauthorizedResponse;
 
 public class ReimbursementController implements Controller {
 
+    private JWTService jwtService;
+
+    public ReimbursementController(){
+        this.jwtService = new JWTService();
+    }
 //    private Handler test = (ctx) -> {
 //        User currentlyLoggedInUser = (User) ctx.req.getSession().getAttribute("current_user");
 //
@@ -20,6 +26,8 @@ public class ReimbursementController implements Controller {
 //    };
 
     private Handler getAllReimbursements = (ctx) -> {
+        String jwt = ctx.header("Authorization");
+                this.jwtService.validateJwt(jwt);
 //        User currentlyLoggedInUser = (User) ctx.req.getSession().getAttribute("current_user");
 //        if(currentlyLoggedInUser != null && currentlyLoggedInUser.getUserRole().equals("manager")){
 //        ctx.json("You are authorized to access the code inside here");
--- src/main/java/com/revature/service/JWTService.java
+++ src/main/java/com/revature/service/JWTService.java
@@ -9,13 +9,23 @@ import java.security.Key;
 
 public class JWTService {
 
-    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private Key key;
 
+    //Instance initialization block
+    //This will run immediately after the cinstruction
+
+    {
+        byte[] secret = "my_secret_password_my_secret_password_my_secret_password_my_secret_password".getBytes();
+            key = Keys.hmacShaKeyFor(secret); //create a key using
+    }
+
+    //Signing a JWT with the key
     public String createJWT(User user){
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
         String jwt = Jwts.builder()
                 .setSubject(user.getUsername())
+                .claim("user_id", user.getId())
                 .claim("user_role", user.getUserRole())
                 .signWith(key)
                 .compact();
@@ -23,11 +33,21 @@ public class JWTService {
     return jwt;
     }
 
+    //validating a JWT with the key
     public void validateJwt(String jwt){
         try{
             Jws<Claims> token = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
-            System.out.println(token.getBody());
+//            System.out.println(token.getBody());
+            String username = token.getBody().getSubject();
+            Integer userId = token.getBody().get("user_id", Integer.class);
+            String userRole = token.getBody().get("user_role",String.class);
+
+            System.out.println(username);
+            System.out.println(userId);
+            System.out.println(userRole);
+
         }catch(JwtException e){
+            e.printStackTrace();
             throw new UnauthorizedResponse("JWT was invalid");
         }
     }
